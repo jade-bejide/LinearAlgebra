d00c3fdd5c57dc4ffaeb29d7b0f0be9259af8851
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-02-19 17:13:40 +0000

    Completed getTriangular Form
    Next find unique values

--- GaussianElimination.java
+++ GaussianElimination.java
@@ -1,8 +1,8 @@
 package LinearAlgebra;
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.ArrayList;
 
 public class GaussianElimination {
     static Scanner mBym = new Scanner(System.in);
@@ -65,39 +65,52 @@ public class GaussianElimination {
     }
 
     public Integer[][] buildAugMatrix(int n, Integer[][] a, Integer[] b) {
-        Integer[][] augementedMatrix = new Integer[n][n+1];
+        Integer[][] augmentedMatrix = new Integer[n][n+1];
         int row = 0;  
 
         while (row < a.length) {
             int column = 0;
             while (column < a[row].length) {
-                augementedMatrix[row][column] = a[row][column];
+                augmentedMatrix[row][column] = a[row][column];
                 column++;
             }
 
-            augementedMatrix[row][column] = b[row];
+            augmentedMatrix[row][column] = b[row];
             row++;
         }
 
-        return augementedMatrix;
+        return augmentedMatrix;
     }
 
-    public Integer[][] solveGE(Integer[][] matrix) {
-        Integer[] pivot = matrix[0];
-        int row = 1;
-
+    public Integer[][] getTriangularForm(Integer[][] matrix) {
+        int row = 0;
+        int column = 0;
         while (row < matrix.length) {
-            Integer multiplier = matrix[row][0] / pivot[0];
-
-            int elem = 0;
-            while (elem < matrix[row].length) {
-                matrix[row][elem] = matrix[row][elem] - (multiplier * pivot[elem]);
-                elem++;
+            Integer[] pivot = matrix[row];
+    
+            int feederRow = row + 1;
+            while (feederRow < matrix.length) {
+                Integer multiplier = matrix[feederRow][column] / pivot[column];
+                int elem = 0;
+                while (elem < matrix[feederRow].length) {
+                    matrix[feederRow][elem] = matrix[feederRow][elem] - (multiplier * pivot[elem]);
+                    elem++;
+                }
+                feederRow++;
+                
             }
             row++;
+            column++;
         }
 
         return matrix;
+    }
+
+    public Integer[][] solveGE(Integer[][] matrix) {
+        Integer[][] triForm = getTriangularForm(matrix);
+
+
+        return triForm;
 
     }
 
@@ -116,6 +129,17 @@ public class GaussianElimination {
         } 
     }
 
+    public void printMatrix(int n, Integer[] matrix) {
+        int column = 0;
+        System.out.print("[ ");
+        while (column < n) {
+            System.out.print(matrix[column] + " "); 
+                column++;
+        }
+        System.out.print("]");
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         GaussianElimination system = new GaussianElimination();
         int elems = system.getUnknowns();
@@ -131,11 +155,15 @@ public class GaussianElimination {
         System.out.println("Building Known Column Vector");
         Integer[] v = system.parseRC(elems);
 
-        Integer[][] augumentedMatrix = system.buildAugMatrix(elems, a, v);
+        Integer[][] augmentedMatrix = system.buildAugMatrix(elems, a, v);
         System.out.println("Matrix: ");
         system.printMatrix(elems, elems, a);
         System.out.println("Augmented Matrix: ");
-        system.printMatrix(elems, elems+1, augumentedMatrix);
+        system.printMatrix(elems, elems+1, augmentedMatrix);
+
+        System.out.println("Triangular Form: ");
+        Integer[][] round1 = system.solveGE(augmentedMatrix);
+        system.printMatrix(elems, elems+1, round1);
 
 
         b.close();
